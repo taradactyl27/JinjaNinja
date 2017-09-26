d1345e9fe1f4f68e18e666fe017debec2a4680a1
taradactyl27 <ataradachuk@gmail.com>
2017-09-26 14:00:43 -0400

    done

--- rand.py
+++ rand.py
@@ -1,9 +1,13 @@
 import random
+from flask import Flask, render_template
+myapp = Flask(__name__)
+    
 filename = 'occupations.csv'
 f = open(filename,'rU')
 text = f.read()
 
 dict = {}
+dict2 = {}
 L1 = text.split("\n")
 L = filter(None, L1)
 i = 1
@@ -13,13 +17,15 @@ while i < len(L)-1:
         index = L[i].find('"', 1, len(L[i]))
         count += float(L[i][index+2:]) * 10
         dict[count] = L[i][1:index]
+        dict2[L[i][1:index]] = float(L[i][index+2:]) 
         i += 1
     else:
         line = L[i].split(",")
         count += float(line[1]) * 10
         dict[count] = line[0]
+        dict2[line[0]] = float(line[1]) 
         i += 1
-        
+
 def rand():
     ans = 0
     number = random.randint(1,998)
@@ -30,6 +36,18 @@ def rand():
         elif number <= thing:
             if thing < ans:
                 ans = thing
-    print dict[ans]
+    return dict[ans]
 
-rand()
+@myapp.route('/')
+def root():
+    return render_template("template.html")
+
+@myapp.route('/occupations')
+def occup():
+    return render_template("occupations.html", occupation = dict2, randoccupation = rand())
+
+if __name__ == '__main__':
+    myapp.debug = True
+    myapp.run()
+    
+
